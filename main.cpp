1e6b41ef79704b7b80409edaed31f30bcd7620ce
Tanay Daware <tanaydaware2210@gmail.com>
2026-08-13 19:42:59 +0530

    Updated main.cpp for help function
    Their was no code for the help function in the main file added the code for CLIParser::help()

--- main.cpp
+++ main.cpp
@@ -1,1 +1,13 @@
-int main() { return 0; }
+#include "CLIParser.h"
+
+int main(int argc, char* argv[]) {
+    CLIParser cli;
+    cli.parse(argc, argv);
+
+    if (!cli.hasCommand() || cli.getCommand() == "help") {
+        cli.printHelp();
+        return 0;
+    }
+
+    return 0;
+}
